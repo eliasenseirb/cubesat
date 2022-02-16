f0d8937ef68d122d6be93694de3a71a5edf73308
Charles Guitton <charles.guitton@bordeaux-inp.fr>
2022-02-16 17:53:54 +0100

    Mod CSS fini

--- Mission 1/fc.m
+++ Mission 1/fc.m
@@ -1,10 +1,11 @@
 function [f_c] = fc(t,gammap,B,Ts)
-    if t>=-Ts/2 && t<-Ts/2+gammap
-        f_c=(B/Ts)*(t-gammap)+B;
-    elseif t>=-Ts/2+gammap && t<=Ts/2
-        f_c=(B/Ts)*(t-gammap);
-    else 
-        f_c=0;
+    f_c=zeros(size(t));
+    for k=1:length(t)
+        if t(k)>=-Ts/2 && t(k)<-Ts/2+gammap
+            f_c(k)=(B/Ts)*(t(k)-gammap)+B;
+        elseif t(k)>=-Ts/2+gammap && t(k)<=Ts/2
+            f_c(k)=(B/Ts)*(t(k)-gammap);
+        end
     end
 end
 
