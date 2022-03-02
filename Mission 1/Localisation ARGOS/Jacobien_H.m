df4768af25a9c87d9e6a3361eb4b75efad7491df
Guiguishop <71508215+Guiguishop@users.noreply.github.com>
2022-03-02 08:37:07 +0100

    dossiers

---
+++ Mission 1/Localisation ARGOS/Jacobien_H.m
@@ -0,0 +1,68 @@
+function [J_H]=Jacobien_H(lambda,phi,h,ft)
+%Jacobienne de la fonction d'observation Doppler H
+
+tau = 120e-3;   % durée d'une mesure
+RE = 6378.137e3;                                  % Taille du demi grand axe en m
+f = 1/298.257223563;                              % Aplatissement de l'ellipsoide
+RP = RE*(1-f);                                    % Valeur du demi-petit axe 
+                                          % Vitesse du satellite en m/s
+c=physconst('LightSpeed');                        % Célérité de la lumière en m/s
+GE=RE+h; 
+GP=RP+h;
+theta = atan((GP/GE)*tan(phi));                   % Latitude paramétrique (ou réduite)
+
+% Coordonnées cartésiennes
+x=GE*cos(theta)*cos(lambda); 
+y=GE*cos(theta)*sin(lambda);
+z=GP*sin(theta);
+
+% informations balise 
+hb=0; %altitude
+phib=44.8059; %Latitude (Bordeaux)
+lambdab=-0.605349;% Longitude (Bordeaux)
+
+GEb=RE+hb;
+GPb=RP+hb;
+thetab = atan((GP/GE)*tan(phib));
+
+% coordonnées balise
+xb=GEb*cos(thetab)*cos(lambdab); % x dans un repère cartésien 
+yb=GEb*cos(thetab)*sin(lambdab);
+zb=GPb*sin(thetab);
+vb=1.5; % vitesse balise
+
+% informations satellite : 
+hs=800e3;               %altitude
+phis=44.8059;           %Latitude (Satellite)
+lambdas=-0.605349;      % Longitude (Satellite)
+
+GEs=RE+hs;
+GPs=RP+hs;
+thetas = atan((GPs/GEs)*tan(phis));
+
+% coordonnées cartesiennes satellite
+xs=GEs*cos(thetas)*cos(lambdas); 
+ys=GEs*cos(thetas)*sin(lambdas);
+zs=GPs*sin(thetas);
+vs=7e3;                 %vitesse du satellite
+
+zsf = zs + vs*tau;              %coordonnée selon z du satellite en fin de comptage
+xsf = xs;                       %coordonnées en x et y sont inchangées puisque le satellite ne bouge que selon z
+ysf = ys;
+
+Rf= sum([xsf ysf zsf].*[xb yb zb]); %distance satellite-balise en fin de comptage
+Rd= sum([xs ys zs].*[xb yb zb]);    %distance satellite-balise en début de comptage
+vr = (Rf-Rd)/tau;                   % approximation de la vitesse radiale su satellite
+
+dH_dxyz = (ft/c*tau)*[((x-xsf)/Rf-(x-xs)/Rd) ((y-ysf)/Rf-(y-ys)/Rd) ((z-zsf)/Rf-(z-zs)/Rd)];
+
+dH_dft = 1- vr/c;
+
+dtheta_dphi = (GP/GE)/((1-GP^2/GE^2)*cos(phi)^2+GP^2/GE^2);
+
+dxyz_dlambdaphih = [-GE*cos(theta)*sin(lambda) -GE*sin(theta)*cos(lambda)*dtheta_dphi cos(theta)*sin(lambda);GE*cos(theta)*cos(lambda) -GE*sin(theta)*sin(lambda)*dtheta_dphi cos(theta)*sin(lambda);0 GP*cos(theta)*dtheta_dphi sin(theta)];
+
+J_H = [dH_dxyz*dxyz_dlambdaphih dH_dft];
+
+
+end
---
+++ Mission 1/Modulations/DCSS.m
@@ -0,0 +1,89 @@
+clear;
+close all;
+clc
+
+%% Paramètres
+SF = 7 ;            %Nombre de bits/symbole
+M=2^SF;
+
+B=600e3;            % Largeur de bande
+P= 14;              %Puissance du signal émis (en Dbm)
+Ts=M/B;            %Temps symbole
+Ds = 1/Ts;         %Debit symbole
+Te = Ts/M;        %Période d'échantillonnage
+Nb_preambule_up = 5;
+Nb_preambule_down=2;
+Nb_Chirp = 10;
+SNR_dB = 40;           %Rapport signal sur bruit au niveau du récepteur
+Nbbits = SF*Nb_Chirp;     %Nombre de bits générés
+time = -Ts/2:Te:Ts/2-Te;                % base de temps sur laquelle les chirps sont générés
+
+%% Transmetteur
+sb = randi([0,1],1,Nbbits);
+chirp_up= exp(1j*2*pi.*time*B/Ts.*time);    % Chirp up
+chirp_down= exp(-1j*2*pi.*time*B/Ts.*time);     %Chirp down
+sbMAT = reshape(sb,SF,length(sb)/SF);           %Matrice dont les colonnes sont des sous-sequences de SF bits
+
+Sp = bit2int(sbMAT,SF,true);                    %Convertit en decimal les sequences de SF bits avec bit de poids fort à gauche (en haut de la colonne)
+Dp = zeros(size(Sp));
+for k=1:length(Sp)
+    if k~=1
+        Dp(k) = mod(Dp(k-1)+Sp(k),M);
+    else
+        Dp(k) = mod(Sp(k),M);
+    end
+end
+Dp=[0,Dp];
+gammap = Dp/B;
+
+preambule=[repmat(chirp_up,1,Nb_preambule_up), repmat(chirp_down,1,Nb_preambule_down)]; % Signal d'apprentissage (header loRa)
+s=[];
+for k=1:length(gammap)
+    s = [s exp(1j*2*pi.*time.*fc(time,gammap(k),B,Ts))];
+end
+s=[preambule s];
+%% Canal
+h=1;
+
+y=filter(h,1,s);
+
+%% Récepteur
+
+Py = mean(abs(y).^2); % Puissance instantanée du signal reçu
+Pbruit = Py/10^(SNR_dB/10);
+%Pbruit=0;
+b = sqrt(Pbruit/2) * (randn(size(y)) + 1i*randn(size(y))); % vecteur de bruit AWG de variance Pbruit
+
+x = y + b;
+
+temp=floor(length(x)/M); % Durée d'un chirp
+x=x(1:temp*M); % on redimensionne x pour le reshape
+
+sig_reshaped=reshape(x,[M,temp]); % on met en colonne les chirps
+z=sig_reshaped.*chirp_up'; % multiplication par le chirp brut
+
+[max_fft, symbolesEstLoRa]=max(abs(fft(z, M, 1))); % argmax des FFT
+symbolesEstLoRa = M-(symbolesEstLoRa(8:end)-1) ;% symboles estimés sans le préambule
+% Amélio concavité
+[symbole,maxi]= concave(z(:,8:end),symbolesEstLoRa,M); % amélioration de la localisation des max
+for k=1:length(symbolesEstLoRa)-1
+    symboleEst(k) =mod(symbolesEstLoRa(k+1)-symbolesEstLoRa(k),M); 
+    new_symb_est(k)=round(mod(symbole(k+1)-symbole(k),M));
+end
+
+BER = mean(abs(Sp-symboleEst));
+
+bit_est = int2bit(symboleEst,SF);%bits estimés sans le préambule
+bit_est2 = int2bit(new_symb_est,SF);%bits estimés sans le préambule
+BER = mean(abs(sb-bit_est(:)')); % BER avec méthode "classique"
+BER2=mean(abs(sb-bit_est2(:)')); % BER avec l'algo de concavité
+
+%% Figures
+
+
+figure,
+subplot 211
+plot(abs(s)),title("Module de s")
+subplot 212
+plot(angle(s)),title("Phase de s")
+
