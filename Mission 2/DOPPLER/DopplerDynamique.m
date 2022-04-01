3b9961b95e2727aa8d35c5727e58427e5ae1677c
Yahya Mesmoudi <ymesmoudi@enseirb-matmeca.fr>
2022-04-01 15:51:22 +0200

    Doppler

--- Mission 2/DOPPLER/DopplerDynamique.m
+++ Mission 2/DOPPLER/DopplerDynamique.m
@@ -1,43 +1,109 @@
-clc;
-clear ;
+clc
+clear;
 close all;
-%% Parametres "constants"
+addpath("..\..\satellite_trajectory_link_budget")
+format long;
+warning('off','all');
 
+%%% Estimation de l'évolution de l'effet Doppler en foction du temps pour
+%%% à la reception depuis un site au sol et determination du Doppler rate
+
+%% Localisation du site d'émission
+Site_Lat=30;
+Site_Long=30;
+
+%% Constantes
+R=6378;                                                                     % Rayon de la terre en km
+u =3998600.4415;                                                            % Gravitational parameter
 fT=2.4e9;                                                                   % frequence de la porteuse
 Vs=7;                                                                       % Vitesse du sattelite en Km/s par rapport à la surface de la terre
 c=299792.458;                                                               % Vitesse du la lumière en Km/s 
-H=500;                                                                      % Altitude du sattelite par rapport au centre de la terre (en KM)
-R=6371;                                                                     % Rayon de la terre (en KM)
+
+%% Parametres sur la trajectoire du satellite
+H=500 ;                                                                     % satellite attitude (km)
+a=H+R ;                                                                     % Semimajor axis in Km
+e=0;                                                                        % Eccentricity 
+i=90;                                                                       % Inclination
+omega=90;                                                                   % Right ascension of the ascending node
+w=90;                                                                       % Argument of perigee
+theta=90;                                                                   % Initial True anomaly
+eps=20;                                                                     % minimum elevation angle in degrees
+UTC='08-sep-2020 17:46:16';                                                 % Satellite launch time
+latmin=-90;
+latmax=90;
+T=(2*pi)*(sqrt((a^3)/u));                                                   %The Satellite period around the Earth in seconds
 
 %% Parametres de la simulation
+steps=100;                                                                  %Précision de la simulation 
+dT=T/steps;                                                                 %Time step
+N=1;                                                                        % number of turns around earth
+T_f=N*T;                                                                    %24h
+time = 0:dT:T_f/2-dT;
 
-Latitude=90;                                                                % La latitude du site d'emission en degrée (angle positif (0 --> 90) pour hémisphère nord
-                                                                            % et negatif (0 --> -90) pour l'hémisphère sud) 
-Theta_i=0;                                                                  % La position initiale du sattelite
-Tmax=500000;
+%% Simulation 
+disp('The Simulator is running satrack.m function');
+[S_lat, S_long, Ecc, E_time] = satrackFoV(a, e, i, omega, w, theta, UTC,time,T);
 
-Theta_M=90-Latitude;
 
-%% simulation 
+S_long=S_long+180;
 
-t=0:1:Tmax;
-ThetaS=Vs*t/(R+H)+Theta_i;
+%%
 
-%alpha = atand(cosd(ThetaS-Theta_M)./(1+(R*tand(ThetaS-Theta_M)./(cosd(Theta_M)*((R+H)./cosd(ThetaS)-R./(cosd(Theta_M)*cosd(ThetaS-Theta_M)))))));
-%fR=fT*(1-Vs*cosd(alpha)/c);
-%EffetDoppler=fR-fT;
-%figure,
-%plot(t,EffetDoppler/1000,"r");
-%xlabel('temps (s)')
-%ylabel('Décalage Doppler en module (khz)')
-%title('Décalage Doppler en fontion du temps pour un site de latitude 90°')
 
-alphaTest=atand(((R+H)*cosd(ThetaS)-R*cosd(Theta_M))./(cosd(Theta_M)*R));
-alpha=180-alphaTest+Theta_M+ThetaS;
-fR=fT*(1-Vs*cosd(alpha)/c);
+
+E = ElevationAngle2(S_long,S_lat,Site_Long,Site_Lat,H);
+fR=fT*(1-Vs*cosd(E)/c);
 EffetDoppler=fR-fT;
 figure,
-plot(t,EffetDoppler/1000,"r");
+plot(time,EffetDoppler,"r");
 xlabel('temps (s)')
-ylabel('Décalage Doppler en module (khz)')
-title('Décalage Doppler en fontion du temps pour un site de latitude 0° : début pire cas')
+ylabel('Décalage Doppler (hz)')
+title("Décalage Doppler pour un site de latitude 30° et de longitude 30° lors d'une demi periode polaire")
+
+figure,
+
+%bordeaux
+Site_Lat=44.8378;
+Site_Long=-0.594;
+E1 = ElevationAngle2(S_long,S_lat,Site_Long,Site_Lat,H);
+fR=fT*(1-Vs*cosd(E1)/c);
+EffetDoppler1=fR-fT;
+Site_Lat=0;
+Site_Long=0;
+E2 = ElevationAngle2(S_long,S_lat,Site_Long,Site_Lat,H);
+fR=fT*(1-Vs*cosd(E2)/c);
+EffetDoppler2=fR-fT;
+hold on
+plot(time,EffetDoppler1);
+plot(time,EffetDoppler);
+plot(time,EffetDoppler2);
+hold off
+xlabel('temps (s)')
+ylabel('Décalage Doppler (hz)')
+legend('site à Bordeaux','site de latitude 30° et de longitude 30°','site de latitude 0° et de longitude 0°')
+title("Décalage Doppler pour différent sites d'émission lors d'une demi periode polaire")
+
+
+time = time(1:end-1);
+figure,
+plot(time,(EffetDoppler(2 :end)-EffetDoppler(1 :end-1))/dT,"r");
+xlabel('temps (s)')
+ylabel('Doppler rate (hz/s)')
+title("Doppler rate pour un site de latitude 30° et de longitude 30° lors d'une demi periode polaire")
+
+figure,
+hold on
+plot(time,(EffetDoppler1(2 :end)-EffetDoppler1(1 :end-1))/dT);
+plot(time,(EffetDoppler(2 :end)-EffetDoppler(1 :end-1))/dT);
+plot(time,(EffetDoppler2(2 :end)-EffetDoppler2(1 :end-1))/dT);
+hold off
+xlabel('temps (s)')
+ylabel('Doppler rate (hz/s)')
+legend('site à Bordeaux','site de latitude 30° et de longitude 30°','site de latitude 0° et de longitude 0°')
+title("Doppler rate pour différent sites d'émission lors d'une demi periode polaire")
+
+DureePaquet=0.01774;
+VariationMaxDoppler=max(abs((EffetDoppler1(2 :end)-EffetDoppler1(1 :end-1))/dT))*DureePaquet
+
+
+
